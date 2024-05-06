f146921f1b43abae3aef0c19f9c374a18eeb38bd
micheledic <dicroce.m@stesi.eu>
2024-05-06 15:25:57 +0200

    fix

--- controllers/main.py
+++ controllers/main.py
@@ -5,12 +5,13 @@ from odoo import http, _
 from odoo.http import request
 from odoo.tools import float_round
 import datetime
-
-class HrAttendance(http.Controller):
+from odoo.addons.hr_attendance.controllers.main import HrAttendance as HrAttendanceOdoo
+class HrAttendance(HrAttendanceOdoo):
     @http.route('/hr_attendance/manual_selection', type="json", auth="public")
     def manual_selection(self, token, employee_id, pin_code):        
         company = self._get_company(token)        
         if company:
+            #raise Exception(123)
             employee = request.env['hr.employee'].sudo().browse(employee_id)
             if employee.company_id == company and ((not company.attendance_kiosk_use_pin) or (employee.pin == pin_code)):
                 employee.with_company(company.id).sudo()._attendance_action_change(self._get_geoip_response('kiosk'))
